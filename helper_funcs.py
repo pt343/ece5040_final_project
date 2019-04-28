f1754416a31b62e592484f9ab23aa51e041ff434
mbobbett <mb2567@cornell.edu>
2019-04-28 16:10:47 -0400

    updated helper funcs

--- helper_funcs.py
+++ helper_funcs.py
@@ -181,3 +181,63 @@ def extract_features_test(datafolderpath, patient, *argv):
         dict_test["feature{0}".format(arg)]=testfeatures  
     
     return dict_test
+
+def get_train(patient):
+        readin_ict=scipy.io.loadmat('patient_'+str(patient)+'_ict_train')
+        readin_nonict=scipy.io.loadmat('patient_'+str(patient)+'_nonict_train')
+        
+        ict_key=list(readin_ict.keys())
+        nonict_key=list(readin_nonict.keys())
+        
+        ict_label=np.ones(int(readin_ict[ict_key[3]].shape[1]))
+        nonict_label=np.zeros(int(readin_nonict[nonict_key[3]].shape[1]))
+        labels=np.concatenate((ict_label,nonict_label))
+        
+        train_ict=np.asarray([readin_ict[ict_key[3]][0], 
+               readin_ict[ict_key[4]][0],
+               readin_ict[ict_key[5]][0],
+               readin_ict[ict_key[6]][0][::2],
+               readin_ict[ict_key[6]][0][1::2]])
+        train_ict= np.transpose(train_ict)
+        
+        
+        train_nonict=np.asarray([readin_nonict[nonict_key[3]][0], 
+               readin_nonict[nonict_key[4]][0],
+               readin_nonict[nonict_key[5]][0],
+               readin_nonict[nonict_key[6]][0][::2],
+               readin_nonict[nonict_key[6]][0][1::2]])
+        train_nonict= np.transpose(train_nonict)
+        
+        train= np.concatenate((train_ict, train_nonict))
+        
+        return train, labels
+    
+def get_val(patient):
+    readin_ict=scipy.io.loadmat('patient_'+str(patient)+'_ict_val')
+    readin_nonict=scipy.io.loadmat('patient_'+str(patient)+'_nonict_val')
+        
+    ict_key=list(readin_ict.keys())
+    nonict_key=list(readin_nonict.keys())
+        
+    ict_label=np.ones(int(readin_ict[ict_key[3]].shape[1]))
+    nonict_label=np.zeros(int(readin_nonict[nonict_key[3]].shape[1]))
+    labels=np.concatenate((ict_label,nonict_label))
+        
+    val_ict=np.asarray([readin_ict[ict_key[3]][0], 
+               readin_ict[ict_key[4]][0],
+               readin_ict[ict_key[5]][0],
+               readin_ict[ict_key[6]][0][::2],
+               readin_ict[ict_key[6]][0][1::2]])
+    val_ict= np.transpose(val_ict)
+        
+        
+    val_nonict=np.asarray([readin_nonict[nonict_key[3]][0], 
+               readin_nonict[nonict_key[4]][0],
+               readin_nonict[nonict_key[5]][0],
+               readin_nonict[nonict_key[6]][0][::2],
+               readin_nonict[nonict_key[6]][0][1::2]])
+    val_nonict= np.transpose(val_nonict)
+        
+    val= np.concatenate((val_ict, val_nonict))
+    
+    return val, labels
