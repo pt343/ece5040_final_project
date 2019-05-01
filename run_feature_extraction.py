8752b2f8a51fd66b70f64c8f6defd573261398e5
mbobbett <mb2567@cornell.edu>
2019-05-01 13:27:18 -0400

    updated

--- run_feature_extraction.py
+++ run_feature_extraction.py
@@ -8,21 +8,21 @@ from helper_funcs import *
 
 
 
-ft1 = channel_line_length
-ft2 = get_energy
-ft3 = get_variance
-ft4 = get_power_spec
+ft1 = get_skew
+ft2 = get_mean
+ft3 = get_kurtosis
+#ft4 = get_power_spec
 
 patient = 1
 datafolderpath = '/Volumes/TOSHIBA EXT/'
 
-dt = extract_features_test(datafolderpath, patient, ft1,ft2,ft3,ft4)
-d1, d2, d3, d4 = extract_features_train(datafolderpath, patient, ft1,ft2,ft3,ft4)
+dt = extract_features_test(datafolderpath, patient, ft1,ft2,ft3)
+d1, d2, d3, d4 = extract_features_train(datafolderpath, patient, ft1,ft2,ft3)
     
 
 
-scipy.io.savemat('patient_'+str(patient)+'_ict_train', d1)
-scipy.io.savemat('patient_'+str(patient)+'_ict_val', d2)
-scipy.io.savemat('patient_'+str(patient)+'_nonict_train', d3)
-scipy.io.savemat('patient_'+str(patient)+'_nonict_val', d4)
-scipy.io.savemat('patient_'+str(patient)+'_test', dt)
+scipy.io.savemat('patient_'+str(patient)+'_ict_train2', d1)
+scipy.io.savemat('patient_'+str(patient)+'_ict_val2', d2)
+scipy.io.savemat('patient_'+str(patient)+'_nonict_train2', d3)
+scipy.io.savemat('patient_'+str(patient)+'_nonict_val2', d4)
+scipy.io.savemat('patient_'+str(patient)+'_test2', dt)
